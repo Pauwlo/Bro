38f65693f705fb7ff36799c449401ac04da2c2eb
Pauwlo <dev@pauwlo.fr>
2022-11-05 12:23:44 +0100

    Update Chocolatey URL

--- Install/Init/Files/Post install.ps1
+++ Install/Init/Files/Post install.ps1
@@ -82,7 +82,7 @@ if ($ShouldInstallChocolatey) {
 
     Set-ExecutionPolicy Bypass -Scope Process -Force
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     
     $DocumentsPath = [Environment]::GetFolderPath('MyDocuments')
     (Get-Item "$DocumentsPath\WindowsPowerShell").Attributes += 'Hidden'
