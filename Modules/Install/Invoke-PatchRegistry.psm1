a464821b211dc2e102008a051c89f0634a5de98c
Pauwlo <dev@pauwlo.fr>
2023-10-18 21:27:15 +0200

    Check if registry key exists before granting permissions

--- Modules/Install/Invoke-PatchRegistry.psm1
+++ Modules/Install/Invoke-PatchRegistry.psm1
@@ -1,6 +1,9 @@
 function Invoke-PatchRegistry {
-	Grant-KeyPermissions 'HKLM' 'SOFTWARE\Microsoft\Windows\CurrentVersion\Communications'
-
+	$CommunicationsKey = 'SOFTWARE\Microsoft\Windows\CurrentVersion\Communications'
+	if (Test-Path "HKLM:\$CommunicationsKey") {
+		Grant-KeyPermissions 'HKLM' $CommunicationsKey
+	}
+	
 	$FilePath = "$env:TMP\Bro_Tweaks.reg"
 	Get-Asset RegistryFile $FilePath | Out-Null
 	reg import $FilePath 2>&1 | Out-Null
