ca09fd140c3d6a94cdef27c818702fdf60c2e21d
Pauwlo <dev@pauwlo.fr>
2023-11-02 04:24:58 +0100

    Re-patch registry after next restart

--- Modules/Install/Invoke-PatchRegistry.psm1
+++ Modules/Install/Invoke-PatchRegistry.psm1
@@ -7,7 +7,19 @@ function Invoke-PatchRegistry {
 	$FilePath = "$env:TMP\Bro_Tweaks.reg"
 	Get-Asset RegistryFile $FilePath | Out-Null
 	reg import $FilePath 2>&1 | Out-Null
-	Remove-Item $FilePath
+
+	$ScriptFilePath = "$env:TMP\Bro (re-patch registry on reboot).ps1"
+
+	Write-Output "reg import '$FilePath' 2>&1" | Out-File $ScriptFilePath -Encoding ascii
+	Write-Output "Remove-Item '$FilePath'" | Out-File $ScriptFilePath -Append -Encoding ascii
+	Write-Output "Remove-Item '$ScriptFilePath'" | Out-File $ScriptFilePath -Append -Encoding ascii
+	Write-Output "Unregister-ScheduledTask 'Bro\Re-patch registry'" | Out-File $ScriptFilePath -Append -Encoding ascii
+
+	$A = New-ScheduledTaskAction -Execute 'powershell' -Argument "-ExecutionPolicy Bypass -File `"$ScriptFilePath`""
+	$T = New-ScheduledTaskTrigger -AtLogon
+	$P = New-ScheduledTaskPrincipal -UserId SYSTEM -RunLevel Highest
+	$Task = New-ScheduledTask -Action $A -Trigger $T -Principal $P
+	Register-ScheduledTask 'Bro\Re-patch registry' -InputObject $Task | Out-Null
 
 	Stop-Process -Name explorer
 }
