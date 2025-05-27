7fe2e6c25593d8fc694218069df33bdc9328c142
Pauwlo <dev@pauwlo.fr>
2025-05-28 00:01:45 +0200

    Silence Microsoft Store install

--- Modules/Install/Invoke-AddMicrosoftStore.psm1
+++ Modules/Install/Invoke-AddMicrosoftStore.psm1
@@ -1,7 +1,7 @@
 function Invoke-AddMicrosoftStore {
 	$DesktopPath = [Environment]::GetFolderPath('Desktop')
 
-	New-Shortcut "$DesktopPath\Install Microsoft Store.lnk" 'wsreset' '-i' | Out-Null
+	New-Shortcut "$DesktopPath\Install Microsoft Store.lnk" 'wsreset' '-i >nul' | Out-Null
 	
 	New-Shortcut "$DesktopPath\Get App Installer.url" 'ms-windows-store://pdp/?ProductId=9nblggh4nns1'
 	New-Shortcut "$DesktopPath\Get Calculator.url" 'ms-windows-store://pdp/?ProductId=9wzdncrfhvn5'
