8f6d540c4590b2b2c5e5e7f21cb90d6099968655
Pauwlo <dev@pauwlo.fr>
2026-02-19 08:42:30 +0100

    Only add MS Store shortcuts if not installed

--- Modules/Install/Invoke-AddMicrosoftStoreShortcuts.psm1
+++ Modules/Install/Invoke-AddMicrosoftStoreShortcuts.psm1
@@ -1,9 +1,31 @@
 function Invoke-AddMicrosoftStoreShortcuts {
 	$DesktopPath = [Environment]::GetFolderPath('Desktop')
 
-	New-Shortcut "$DesktopPath\Install Microsoft Store.lnk" 'wsreset' '-i >nul' | Out-Null
+	$AppxAvailable = [bool](Get-Command Get-AppxPackage -ErrorAction SilentlyContinue)
 
-	New-Shortcut "$DesktopPath\Get App Installer.url" 'ms-windows-store://pdp/?ProductId=9nblggh4nns1'
-	New-Shortcut "$DesktopPath\Get Calculator.url" 'ms-windows-store://pdp/?ProductId=9wzdncrfhvn5'
-	New-Shortcut "$DesktopPath\Get Webp Image Extensions.url" 'ms-windows-store://pdp/?ProductId=9pg2dk419drg'
+	function Test-AppxInstalled($Name) {
+		if ($AppxAvailable) {
+			[bool](Get-AppxPackage -Name $Name -ErrorAction SilentlyContinue)
+		} else {
+			[bool](Get-ChildItem "$env:ProgramFiles\WindowsApps\$Name*" -ErrorAction SilentlyContinue)
+		}
+	}
+
+	$StoreInstalled = Test-AppxInstalled "Microsoft.WindowsStore"
+
+	if (!$StoreInstalled) {
+		New-Shortcut "$DesktopPath\Install Microsoft Store.lnk" 'wsreset' '-i >nul' | Out-Null
+	}
+
+	if (!$StoreInstalled -or !(Test-AppxInstalled "Microsoft.DesktopAppInstaller")) {
+		New-Shortcut "$DesktopPath\Get App Installer.url" 'ms-windows-store://pdp/?ProductId=9nblggh4nns1'
+	}
+
+	if (!(Test-AppxInstalled "Microsoft.WindowsCalculator")) {
+		New-Shortcut "$DesktopPath\Get Calculator.url" 'ms-windows-store://pdp/?ProductId=9wzdncrfhvn5'
+	}
+
+	if (!(Test-AppxInstalled "Microsoft.WebpImageExtension")) {
+		New-Shortcut "$DesktopPath\Get Webp Image Extensions.url" 'ms-windows-store://pdp/?ProductId=9pg2dk419drg'
+	}
 }
